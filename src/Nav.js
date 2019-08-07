27ba74125284ed6b73bb627495a814f92cbc460d
Hekmat <hekmatkudrat@gmail.com>
2019-08-07 10:12:10 -0700

    added nav bar and links to routes

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
   render() {
     return (
       <div className="jumbotron">
-        
+        <h1>Microblog</h1>
+        <h4>Get in the Rithm of blogging!</h4>
+          <nav className="navbar">
+            <ul className="navbar-nav">
+              <Link to='/'>
+                <li className="nav-item">Blog</li>
+              </Link>
+              <Link to="/new">
+                <li className="nav-item">Add a new post</li>
+              </Link>
+            </ul>
+        </nav>
       </div>
     );
   }
