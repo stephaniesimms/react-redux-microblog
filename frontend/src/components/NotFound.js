215defb1e9f01c233777bf12ae46205db5dbacd8
Stephanie Simms <stephanie.simms@gmail.com>
2019-09-28 13:20:42 -0700

    added styling for NotFound

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-// import "./NotFound.css"
+import Card from 'react-bootstrap/Card';
+import './NotFound.css';
 
 /**
- * NotFound: rendered when page does not match
+ * NotFou;nd: rendered when page does not match
  * any of the routes
  */
 
 class NotFound extends Component {
   render() {
     return (
-      <div className='not-found'>
-        <h3 className='not-found-message'>
-          Oops, we didn't find what you were looking for...
-        </h3>
-        <i className='fas fa-skull-crossbones'></i>
-      </div>
+      <Card className='NotFound-card text-center'
+        bg='dark'
+        text='white'
+        style={{ width: '30rem' }}>
+
+        <Card.Body>
+          <i className='fas fa-skull-crossbones fa-7x' />
+          <Card.Title className='not-found-message pt-3'>
+            Oops, we didn't find what you were looking for...
+        </Card.Title>
+        </Card.Body>
+      </Card>
     );
   }
 }
